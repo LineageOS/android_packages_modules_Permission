1b77732b9a0cfc84b223c946035309a7ff5ed0d1
Danny Lin <danny@kdrag0n.dev>
2021-10-05 19:18:57 -0700

    PermissionController: Enable usage timeline for all permission groups
    Now that we've set the device config to keep discrete app op history for
    all permission groups shown in the privacy dashboard, let's allow users
    to view usage timelines for all of them.
    
    Change-Id: Ia23e3e3ecebd1a9b1268a66bb6d0f571ac32d01a

--- PermissionController/src/com/android/permissioncontroller/permission/ui/model/v31/PermissionUsageControlPreferenceUtils.kt
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/model/v31/PermissionUsageControlPreferenceUtils.kt
@@ -40,7 +40,16 @@ object PermissionUsageControlPreferenceUtils {
         listOf(
             Manifest.permission_group.LOCATION,
             Manifest.permission_group.CAMERA,
-            Manifest.permission_group.MICROPHONE
+            Manifest.permission_group.MICROPHONE,
+            Manifest.permission_group.SENSORS,
+            Manifest.permission_group.CALENDAR,
+            Manifest.permission_group.CALL_LOG,
+            Manifest.permission_group.CONTACTS,
+            Manifest.permission_group.STORAGE,
+            Manifest.permission_group.NEARBY_DEVICES,
+            Manifest.permission_group.PHONE,
+            Manifest.permission_group.ACTIVITY_RECOGNITION,
+            Manifest.permission_group.SMS
         )
 
     @JvmStatic
@@ -115,6 +124,7 @@ object PermissionUsageControlPreferenceUtils {
                 }
                 else -> 0
             }
+        if (act == 0) return
         PermissionControllerStatsLog.write(PERMISSION_USAGE_FRAGMENT_INTERACTION, sessionId, act)
     }
 }
