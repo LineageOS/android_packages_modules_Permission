accda0f85473000547a17d9c73feecee3b0a5343
Nate Myren <ntmyren@google.com>
2025-11-03 10:42:20 -0800

    V2 Merge all permissions in a shared UID for every package in that UID
    Since every package can use the permissions in the shared UID, even if
    the given package didn't request it, every package should be treated in
    UI as if they all have the total set of requested permissions
    
    Test: atest PermissionTest23
    Bug: 435737668
    Flag: EXEMPT CVE_FIX
    Relnote: cve fix
    BUG: 456903810
    Low Coverage Reason: unclear why coverage is not voted +2
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:180c332afe538523cd185aa29d6118a330d44be1
    Merged-In: I886add5db84b659d52d4a60b17304f017a17dc90
    Change-Id: I886add5db84b659d52d4a60b17304f017a17dc90

--- PermissionController/src/com/android/permissioncontroller/pm/data/repository/v31/PackageRepository.kt
+++ PermissionController/src/com/android/permissioncontroller/pm/data/repository/v31/PackageRepository.kt
@@ -23,6 +23,7 @@ import android.graphics.drawable.Drawable
 import android.os.UserHandle
 import android.util.Log
 import com.android.modules.utils.build.SdkLevel
+import com.android.permissioncontroller.permission.data.LightPackageInfoLiveData
 import com.android.permissioncontroller.permission.utils.Utils
 import com.android.permissioncontroller.pm.data.model.v31.PackageAttributionModel
 import com.android.permissioncontroller.pm.data.model.v31.PackageInfoModel
@@ -106,10 +107,13 @@ class PackageRepositoryImpl(
     ): PackageInfoModel? =
         withContext(dispatcher) {
             try {
+                val pm = Utils.getUserContext(app, user).packageManager
                 val packageInfo =
-                    Utils.getUserContext(app, user)
-                        .packageManager
-                        .getPackageInfo(packageName, flags)
+                    LightPackageInfoLiveData.mergePermissionsInSharedUid(
+                        pm.getPackageInfo(packageName, flags),
+                        flags,
+                        pm,
+                    )
                 PackageInfoModel(packageInfo)
             } catch (e: PackageManager.NameNotFoundException) {
                 Log.w(LOG_TAG, "package $packageName not found for user ${user.identifier}")
