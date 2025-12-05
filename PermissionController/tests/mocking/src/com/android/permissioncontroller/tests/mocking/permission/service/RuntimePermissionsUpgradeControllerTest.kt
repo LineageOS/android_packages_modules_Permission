43125d22cbbc10a86fe6b57880230be5bb4c642d
Nate Myren <ntmyren@google.com>
2025-12-05 14:38:26 -0800

    RESTRICT AUTOMERGE Update RuntimePermissionsUpgradeController to accept null PermGroups
    Now that PermStateLiveData can return invalid, the upgrade controller
    needs to handle that.
    
    Test: atest RuntimePermissionsUpgradeControllerTest
    Bug: 435737668
    Relnote: N/A
    Flag: EXEMPT CVE_FIX
    Low-Coverage-Reason: security fix
    (cherry picked from commit 8895a86cf0feb0f05831acee614e44b4eae9970f)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:4d05d29833d2d1a0b15243cc12c37bb3b466f177
    Merged-In: I4e85931d1c8c865694339528265c735cef93dc01
    Change-Id: I4e85931d1c8c865694339528265c735cef93dc01

--- PermissionController/tests/mocking/src/com/android/permissioncontroller/tests/mocking/permission/service/RuntimePermissionsUpgradeControllerTest.kt
+++ PermissionController/tests/mocking/src/com/android/permissioncontroller/tests/mocking/permission/service/RuntimePermissionsUpgradeControllerTest.kt
@@ -78,6 +78,7 @@ import org.mockito.Mockito.`when` as whenever
 import org.mockito.MockitoAnnotations.initMocks
 import org.mockito.MockitoSession
 import org.mockito.quality.Strictness.LENIENT
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class RuntimePermissionsUpgradeControllerTest {
@@ -265,7 +266,7 @@ class RuntimePermissionsUpgradeControllerTest {
                 application,
                 Runnable { completionCallback.complete(Unit) }
             )
-            completionCallback.join()
+            completionCallback.get(30L, TimeUnit.SECONDS)
         }
     }
 
