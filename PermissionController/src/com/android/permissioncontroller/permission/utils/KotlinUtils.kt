LMODroid/platform_packages_modules_Permission

Revert "Revert "Display the system gallery storage permission as..."
Revert submission 35158442-galleryRevert-25

Reason for revert: relanding

Reverted changes: /q/submissionid:35158442-galleryRevert-25
Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:85cbba3826807fadd9ae1eec1c3d7d6a2c5ee5bf
Merged-In: I2e4bb626a2e4b0db71b4d1f63b429b62058c0720
Change-Id: I2e4bb626a2e4b0db71b4d1f63b429b62058c0720

PermissionController/src/com/android/permissioncontroller/permission/utils/KotlinUtils.kt
@@ -738,6 +738,7 @@ object KotlinUtils {
             newPerms,
             group.hasInstallToRuntimeSplit,
             group.specialLocationGrant,
+            group.specialFixedStorageGrant,
         )
     }
 
@@ -858,6 +859,7 @@ object KotlinUtils {
                 newPerms,
                 group.hasInstallToRuntimeSplit,
                 group.specialLocationGrant,
+                group.specialFixedStorageGrant,
             )
         // If any permission in the group is one time granted, start one time permission session.
         if (newGroup.permissions.any { it.value.isOneTime && it.value.isGranted }) {
@@ -1140,6 +1142,7 @@ object KotlinUtils {
                 newPerms,
                 group.hasInstallToRuntimeSplit,
                 group.specialLocationGrant,
+                group.specialFixedStorageGrant,
             )
 
         if (wasOneTime && !anyPermsOfPackageOneTimeGranted(app, newGroup.packageInfo)) {

LineageOS/android_packages_modules_Permission

PermissionController/src/com/android/permissioncontroller/permission/utils/KotlinUtils.kt
@@ -736,7 +736,8 @@ object KotlinUtils {
             group.permGroupInfo,
             newPerms,
             group.hasInstallToRuntimeSplit,
-            group.specialLocationGrant
+            group.specialLocationGrant,
+            group.specialFixedStorageGrant
         )
     }
 
@@ -856,7 +857,8 @@ object KotlinUtils {
                 group.permGroupInfo,
                 newPerms,
                 group.hasInstallToRuntimeSplit,
-                group.specialLocationGrant
+                group.specialLocationGrant,
+                group.specialFixedStorageGrant
             )
         // If any permission in the group is one time granted, start one time permission session.
         if (newGroup.permissions.any { it.value.isOneTime && it.value.isGranted }) {
@@ -1138,7 +1140,8 @@ object KotlinUtils {
                 group.permGroupInfo,
                 newPerms,
                 group.hasInstallToRuntimeSplit,
-                group.specialLocationGrant
+                group.specialLocationGrant,
+                group.specialFixedStorageGrant
             )
 
         if (wasOneTime && !anyPermsOfPackageOneTimeGranted(app, newGroup.packageInfo)) {