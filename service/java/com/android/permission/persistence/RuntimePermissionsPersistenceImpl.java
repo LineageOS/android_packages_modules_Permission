ee30155a8872cd022b5b7fb1983fdcf09e752723
Michael Bestas <mkbestas@lineageos.org>
2026-07-04 07:47:47 +0300

    Revert "permissions: check verity before using"
    This reverts commit cbd1f92737e8421a823bf9e0584a0a7a5b0651e6.
    
    Reason for revert: The apk verity prop is deprecated and this causes
    devices with < R launch sdk to have broken apk verity.
    
    Change-Id: Iab1245b54fca220453ecea87d81ed2733ec6c690

--- service/java/com/android/permission/persistence/RuntimePermissionsPersistenceImpl.java
+++ service/java/com/android/permission/persistence/RuntimePermissionsPersistenceImpl.java
@@ -29,7 +29,6 @@ import android.util.Xml;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.modules.utils.build.SdkLevel;
-import com.android.permission.util.PackageUtils;
 import com.android.server.security.FileIntegrity;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -83,7 +82,7 @@ public class RuntimePermissionsPersistenceImpl implements RuntimePermissionsPers
 
     RuntimePermissionsPersistenceImpl() {
         this(file -> {
-            if (SdkLevel.isAtLeastU() && PackageUtils.isApkVerityEnabled()) {
+            if (SdkLevel.isAtLeastU()) {
                 FileIntegrity.setUpFsVerity(file);
             }
         });
@@ -248,13 +247,11 @@ public class RuntimePermissionsPersistenceImpl implements RuntimePermissionsPers
             Log.e(LOG_TAG, "Failed to write reserve copy: " + reserveFile, e);
         }
 
-        if (PackageUtils.isApkVerityEnabled()) {
-            try {
-                mInjector.enableFsVerity(file);
-                mInjector.enableFsVerity(reserveFile);
-            } catch (Exception e) {
-                Log.e(LOG_TAG, "Failed to verity-protect runtime-permissions", e);
-            }
+        try {
+            mInjector.enableFsVerity(file);
+            mInjector.enableFsVerity(reserveFile);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Failed to verity-protect runtime-permissions", e);
         }
     }
 
--- service/java/com/android/role/persistence/RolesPersistenceImpl.java
+++ service/java/com/android/role/persistence/RolesPersistenceImpl.java
@@ -30,7 +30,6 @@ import android.util.Xml;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.modules.utils.build.SdkLevel;
 import com.android.permission.persistence.IoUtils;
-import com.android.permission.util.PackageUtils;
 import com.android.server.security.FileIntegrity;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -81,7 +80,7 @@ public class RolesPersistenceImpl implements RolesPersistence {
 
     RolesPersistenceImpl() {
         this(file -> {
-            if (SdkLevel.isAtLeastU() && PackageUtils.isApkVerityEnabled()) {
+            if (SdkLevel.isAtLeastU()) {
                 FileIntegrity.setUpFsVerity(file);
             }
         });
@@ -238,13 +237,11 @@ public class RolesPersistenceImpl implements RolesPersistence {
             Log.e(LOG_TAG, "Failed to write reserve copy: " + reserveFile, e);
         }
 
-        if (PackageUtils.isApkVerityEnabled()) {
-            try {
-                mInjector.enableFsVerity(file);
-                mInjector.enableFsVerity(reserveFile);
-            } catch (Exception e) {
-                Log.e(LOG_TAG, "Failed to verity-protect roles", e);
-            }
+        try {
+            mInjector.enableFsVerity(file);
+            mInjector.enableFsVerity(reserveFile);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Failed to verity-protect roles", e);
         }
     }
 
